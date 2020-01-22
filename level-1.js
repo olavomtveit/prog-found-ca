41ca73be62c43402ad375c41dad862e7c0afa26b
olavomtveit <56303134+olavomtveit@users.noreply.github.com>
2020-01-22 11:17:31 +0100

    ma-Lvl1
    Added Question 1-10 (Level 1) for the Module Assignment

---
+++ level-1.js
@@ -0,0 +1,74 @@
+//Question 1
+//var movieTitle = "Frank";
+
+//Question 2
+/* var person = {
+    key: true,
+    cash: 200,
+} */
+
+//Question 3
+/* var outOfStock = true;
+if (outOfStock){
+    console.log("Out of stock");
+}
+else{
+    console.log("In stock")
+} */
+
+//Question 4
+/* var numbers = [7, 2, 19, 3, 14];
+
+for (var i= 0; i < numbers.length; i++){
+    console.log(numbers[i]);
+} */
+
+//Question 5
+/* for (var i = 15; i <= 25; i++) {
+    console.log(i);
+} */
+
+//Question 6
+/* for (var i = 15; i <= 25; i++){
+if (i === 20){
+    console.log(20);
+}
+} */
+
+//Question 7
+/* var actors =[{
+    name: "Mads Mikkelsen",
+    numberOfCredits: 54,
+    oscarNominee: false
+},{
+    name: "Michael Fassbender",
+    numberOfCredits: 63,
+    oscarNominee: true
+}]
+
+for (var i = 0; i < actors.length; i++){
+    console.log(actors[i].numberOfCredits);
+    console.log(actors[i].oscarNominee);
+} */
+
+//Question 8
+/* function whatIDontLike(dontLike){
+    console.log("I don't like" + dontLike);
+}
+whatIDontLike("sweetcorn");  */
+
+//Question 9
+/* function totalNumber(number1, number2){
+    var sum = number1 - number2;
+    console.log(sum);
+}
+totalNumber(43, 7); */
+
+//Question 10
+/* var emptyArray = [];
+
+function addArray(theArray){
+    emptyArray.push("laptop")
+    console.log(emptyArray);
+}
+addArray(); */
